12c1354a6f874ed45b2be5e66cf5b8c94c8a5119
y629 <yuutaishimoto@yahoo.co.jp>
2019-06-17 18:30:40 +0900

    make branch

--- db/migrate/20190616182300_remove_user.rb
+++
@@ -1,4 +0,0 @@
-class RemoveUser < ActiveRecord::Migration[5.2]
-  def change
-  end
-end
--- db/migrate/20190616184841_univs.rb
+++
@@ -1,5 +0,0 @@
-class Univs < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :univs
-  end
-end
