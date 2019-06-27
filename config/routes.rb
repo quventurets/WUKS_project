b2128f6607c34cafa84e59c10ab78c349e40b03f
Taylister <track.and.field.c.love1203@gmail.com>
2019-06-27 16:57:24 +0900

    some modification of list

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   get '/career' => "career#index"
   get '/transfer' => "transfer#index"
-  get '/tansfer/:id' => "transfer#show"
-  get '/transfer/test' => "transfer#test"
-  
-  get '/login' => "login#login"
-  
-  resources :univs
 
+  get '/transfer/:id' => "transfer#show", as: :transfer_show
+
+
+
+
+  
+  get '/login' => "login#login"  
+  resources :univs
   root 'top#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
