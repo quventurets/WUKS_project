b8d0e9f53227ca72f9fb9bbc0539b3f00cd7f013
Taylister <track.and.field.c.love1203@gmail.com>
2019-09-03 00:17:12 +0900

    Complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,65 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Univ.create(
-  [
-   {
-    name: '東京大学',
-    date_general: '7月上旬',
-
-    date_number: 7.0,
-    location: '東京都',
-    otherFac: 0,
-    examtypes: 0,
-   },
-   {
-    name: '東京工業大学',
-    date_general: '8月下旬',
-    date_recommend: '6月中旬',
-    date_number: 8.7,
-    location: '東京都',
-    otherFac: 1,
-    examtypes: 1,
-   },
-   {
-    name: '電気通信大学',
-    date_general: '6月下旬',
-    date_recommend: '6月上旬',
-    date_number: 6.8,
-    location: '東京都',
-    otherFac: 0,
-    examtypes: 1,
-   },
-   {
-    name: '九州大学',
-    date_general: '8月中旬',
-    date_recommend: '6月上旬',
-    date_number: 8.2,
-    location: '福岡県',
-    otherFac: 1,
-    examtypes: 1,
-   },
-   {
-    name: '名古屋大学',
-    date_general: '8月上旬',
-
-    date_number: 8.0,
-    location: '愛知県',
-    otherFac: 1,
-    examtypes: 0,
-   },
-   {
-    name: '大阪大学',
-    date_general: '8月中旬',
-
-    date_number: 8.4,
-    location: '大阪府',
-    otherFac: 1,
-    examtypes: 0,
-   }
-    ]
-)
-
 Comp.create(
   [
    {
