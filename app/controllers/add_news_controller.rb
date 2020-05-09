ed353b622031376982a00eba1edd17f94123bb38
湯之上航 <yunoyunowatawata3@yahoo.co.jp>
2020-05-09 16:43:13 +0900

    更新と削除を追加

--- app/controllers/add_news_controller.rb
+++ app/controllers/add_news_controller.rb
@@ -2,20 +2,45 @@ class AddNewsController < ApplicationController
   layout 'template_listpage.html.erb'
   
   def index
+    @news = News.all.order(date: :desc)
   end
 
   def show 
-    name = params[:title]
+    @news_name = params[:title]
     upload_file = params[:upload]
-    if !upload_file.nil?
-      @news_name = name
-      @news_content = upload_file.read.force_encoding("UTF-8")
-      if !params[:preview]
-        News.create(name: @news_name, content: @news_content, date: Date.today)
-        render :index
-      end
+    @news_content = if !upload_file.nil?
+      upload_file.read.force_encoding("UTF-8")
     else
-      redirect_to action: index, notice: "ファイルを指定してください"
+      nil
+    end
+    @dummy = params[:dummy] != "0"
+
+    
+    if !params[:preview]
+      case params[:upload_type]
+      when "new"
+        if !@news_content.nil?
+          News.create(name: @news_name, content: @news_content, date: Date.today)
+          redirect_to action: :index
+        else
+          redirect_to action: :index, notice: "ファイルを指定してください"
+        end
+      when "upload"
+        news = News.find_by(id: params[:news_id].to_i)
+        news.inspect
+        if !@news_content.nil?
+          news.update(name: @news_name, content: @news_content, date: Date.today)
+        else
+          news.update!(name: @news_name, date: Date.today)
+        end
+        redirect_to action: :index
+      when "disable"
+        news = News.find_by(id: params[:news_id].to_i)
+        news.destroy
+        redirect_to action: :index
+      end
+    elsif @news_content.nil?
+      redirect_to action: :index, notice: "ファイルを指定してください"
     end
   end
 
